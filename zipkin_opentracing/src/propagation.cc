00f5047203f1078b04937e48891de20918791bc3
Ryan Burn <ryan.burn@gmail.com>
2017-07-11 12:36:46 -0400

    Add zipkin field names.

--- zipkin_opentracing/src/propagation.cc
+++ zipkin_opentracing/src/propagation.cc
@@ -3,6 +3,19 @@
 namespace ot = opentracing;
 
 namespace zipkin {
+#define PREFIX_TRACER_STATE "x-b3-"
+// Note: these constants are a convention of the OpenTracing basictracers.
+const ot::string_view prefix_baggage = "ot-baggage-";
+
+const int tracer_state_field_count = 3;
+const ot::string_view zipkin_trace_id = PREFIX_TRACER_STATE "traceid";
+const ot::string_view zipkin_span_id = PREFIX_TRACER_STATE "spanid";
+const ot::string_view zipkin_parent_span_id =
+    PREFIX_TRACER_STATE "parentspanid";
+const ot::string_view zipkin_sampled = PREFIX_TRACER_STATE "sampled";
+const ot::string_view zipkin_flags = PREFIX_TRACER_STATE "flags";
+#undef PREFIX_TRACER_STATE
+
 opentracing::expected<void>
 injectSpanContext(std::ostream &carrier,
                   const zipkin::SpanContext &span_context,
