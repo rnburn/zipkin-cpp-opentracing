ce3fb890202eed0261d065c2fa44e278f3fbcc3f
Ryan Burn <ryan.burn@gmail.com>
2018-03-14 16:52:14 -0400

    Fix bazel installation.

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -13,13 +13,13 @@ apt-get install --no-install-recommends --no-install-suggests -y \
 apt-get install --no-install-recommends --no-install-suggests -y \
          curl \
          ca-certificates \
-         openjdk-8-jdk \
+         openjdk-8-jdk
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
-      | tee /etc/apt/sources.list.d/bazel.list \
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add - \
-apt-get update \
+      | tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y \
-         bazel \
+         bazel
 apt-get upgrade -y bazel
 
 # Build OpenTracing
