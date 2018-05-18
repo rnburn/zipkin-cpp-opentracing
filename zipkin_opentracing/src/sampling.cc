3b29eabc540682b8a418c778befaa33762f6f308
Paul Ingles <paul@oobaloo.co.uk>
2018-05-18 22:44:13 +0100

    use static thread_local random generator

--- zipkin_opentracing/src/sampling.cc
+++ zipkin_opentracing/src/sampling.cc
@@ -1,13 +1,10 @@
 #include "sampling.h"
-#include <random>
 
 namespace zipkin {
   bool
   ProbabilisticSampler::ShouldSample() {
-    std::random_device device;
-    std::mt19937 gen(device());
+    static thread_local std::mt19937 rng(std::random_device{}());
     std::bernoulli_distribution dist(sample_rate_);
-    
-    return dist(gen);
+    return dist(rng);
   }
 } // namespace zipkin
