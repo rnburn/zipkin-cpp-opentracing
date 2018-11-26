c0656aff685ba13d4c92ae7869246411c523891e
Vincent Prince <vincent.prince.external@saftbatteries.com>
2018-11-26 11:47:59 +0100

    add curl timeout

--- zipkin_opentracing/include/zipkin/opentracing.h
+++ zipkin_opentracing/include/zipkin/opentracing.h
@@ -6,6 +6,7 @@ namespace zipkin {
 struct ZipkinOtTracerOptions {
   std::string collector_host = "localhost";
   uint32_t collector_port = 9411;
+  long collector_timeout = DEFAULT_TRANSPORT_TIMEOUT;
   SteadyClock::duration reporting_period = DEFAULT_REPORTING_PERIOD;
   size_t max_buffered_spans = DEFAULT_SPAN_BUFFER_SIZE;
   double sample_rate = 1.0;
