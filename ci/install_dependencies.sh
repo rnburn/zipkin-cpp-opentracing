93c77eae515a2a7123f27d1d5080e74432e1d639
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-11-28 11:13:40 -0300

    Use opentracing-cpp pinned version

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-apt-get update 
+apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y \
                 libcurl4-openssl-dev \
                 build-essential \
@@ -11,7 +11,8 @@ apt-get install --no-install-recommends --no-install-suggests -y \
 
 # Build OpenTracing
 cd /
-git clone https://github.com/opentracing/opentracing-cpp.git
+export OPENTRACING_VERSION=1.5.0
+git clone -b v$OPENTRACING_VERSION https://github.com/opentracing/opentracing-cpp.git
 cd opentracing-cpp
 mkdir .build && cd .build
 cmake -DBUILD_TESTING=OFF ..
